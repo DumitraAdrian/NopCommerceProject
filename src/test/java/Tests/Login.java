dd76380f767b6e5510d1a04c459f3f37a126c205
DumitraAdrian <adrian.danyel92@gmail.com>
2024-02-15 15:59:42 +0200

    commit 3

--- src/test/java/Tests/Login.java
+++ src/test/java/Tests/Login.java
@@ -10,16 +10,7 @@ public class Login extends Hooks {
     @Test
 
     public void metodatest() {
-//        EdgeOptions options = new EdgeOptions();
-//        options.addArguments("--window-size=1920,1080");
-//        driver = new EdgeDriver(options);
-//
-//        driver.get("https://demo.nopcommerce.com/");
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        JavascriptExecutor jse = (JavascriptExecutor) driver;
-//        jse.executeScript("window.scrollBy(0,450)");
+
 
         HomePage homePage=new HomePage(getDriver());
         homePage.clickHomePageLogin();
